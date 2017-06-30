0b1f4e89db0925f79661ff5d636e23652c0266c0
u1409433 <u1409433@users.noreply.github.com>
2017-06-30 20:02:19 +0100

    Made some Stuff

---
+++ BrainsEdenJPop/Assets/Joey/Scripts/JL_Camfollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JL_Camfollow : MonoBehaviour
+{
+    public GameObject PC;
+    // Use this for initialization
+    void Start()
+    {
+        PC = GameObject.Find("PC");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(PC.transform.position.x, 20, PC.transform.position.z);
+    }
+}
