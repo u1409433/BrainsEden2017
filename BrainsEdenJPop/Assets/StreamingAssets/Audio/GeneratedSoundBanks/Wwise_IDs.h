3735bd706f9a1ffcc125ce9b346de8a340b864a6
JohannesGames <j.brunn@hotmail.com>
2017-06-30 23:59:58 +0200

    First audio update (ambience)

---
+++ BrainsEdenJPop/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,53 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID AMBIENCESTOP = 128372033U;
+        static const AkUniqueID PICKUPRELIC = 865029174U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace AMBIENCE
+        {
+            static const AkUniqueID GROUP = 85412153U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ALTAR1 = 2844735168U;
+                static const AkUniqueID ALTAR2 = 2844735171U;
+                static const AkUniqueID ALTAR3 = 2844735170U;
+                static const AkUniqueID EXIT = 26822469U;
+                static const AkUniqueID SPAWN = 71202558U;
+            } // namespace SWITCH
+        } // namespace AMBIENCE
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
