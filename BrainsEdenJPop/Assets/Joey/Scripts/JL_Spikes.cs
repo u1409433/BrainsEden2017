2b90fe6122055894218ff459be4b5c1b89e477e8
u1409433 <u1409433@users.noreply.github.com>
2017-07-01 19:10:47 +0100

    SPIKES

---
+++ BrainsEdenJPop/Assets/Joey/Scripts/JL_Spikes.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JL_Spikes : MonoBehaviour
+{
+    public float FL_SwitchTime;
+    public float FL_Cooldown;
+
+    public bool BL_Up;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (FL_SwitchTime <= Time.time)
+        {
+
+        }
+    }
+}
