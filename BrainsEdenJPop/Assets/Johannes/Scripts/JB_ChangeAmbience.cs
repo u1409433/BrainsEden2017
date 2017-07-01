c36c3e1e39beb96a71590221236b529a3b4af037
JohannesGames <j.brunn@hotmail.com>
2017-07-01 15:15:15 +0200

    Proper wwise file structure

---
+++ BrainsEdenJPop/Assets/Johannes/Scripts/JB_ChangeAmbience.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JB_ChangeAmbience : MonoBehaviour
+{
+    public string SwitchGroupName;
+
+    public string SwitchStateName;
+
+    private GameObject PC;
+
+    void Start()
+    {
+        PC = GameObject.Find("PC");
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject == PC)
+        {
+            AkSoundEngine.SetSwitch(SwitchGroupName, SwitchStateName, PC);
+        }
+    }
+}
