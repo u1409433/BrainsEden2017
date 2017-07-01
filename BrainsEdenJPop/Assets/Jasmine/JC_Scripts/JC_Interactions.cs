9d5eb198b4c3cc629921545a78af6791a89b5f64
u1414346 <u1414346@uel.ac.uk>
2017-07-01 15:52:45 +0100

    OtherThingsAI

---
+++ BrainsEdenJPop/Assets/Jasmine/JC_Scripts/JC_Interactions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JC_Interactions : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
