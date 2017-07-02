060954622aa0a6edf77424d6f55540a2f6b186f3
JohannesGames <j.brunn@hotmail.com>
2017-07-02 16:56:59 +0200

    Sneeekt

---
+++ BrainsEdenJPop/Assets/Johannes/Scripts/CallSnickt.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallSnickt : MonoBehaviour
+{
+    float timer;
+    public float cooldown = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer <= Time.time)
+        {
+            AkSoundEngine.PostEvent("Snickt", gameObject);
+            timer = Time.time + cooldown;
+        }
+    }
+}
